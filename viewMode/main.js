2cd457844c176bb95d989cda55192e4fc0e4e9ae
euphokumiko <euphokumiko.iem06@nctu.edu.tw>
2022-09-29 15:40:36 +0800

    change animation list to checkbox form

--- viewMode/main.js
+++ viewMode/main.js
@@ -1,5 +1,5 @@
-'use strict';
-let app;
+"use strict";
+let app, theSpine;
 const apiLoader = new PIXI.Loader(), dropLoader = new PIXI.Loader(), cont = new PIXI.Container();
 const SML0 = "sml_cloth0", SML1 = "sml_cloth1", BIG0 = "big_cloth0", BIG1 = "big_cloth1";
 
@@ -37,7 +37,7 @@ function dropHandler(event) {
         dropLoader
             .add("dropJson", pathJSON)
             .add("dropAtlas", pathAtlas)
-            .load(function(loader, resources) {
+            .load(function (loader, resources) {
                 renderByDrop(resources.dropAtlas.data, JSON.parse(resources.dropJson.data), pathTexture);
             });
     }
@@ -52,7 +52,6 @@ function dragOverHandler(event) {
 }
 
 function init() {
-    let animationCount = 1;
     const canvas = document.getElementById("canvas"), resetBtn = document.getElementById("resetAnimation");
 
     app = new PIXI.Application({
@@ -71,7 +70,7 @@ function init() {
 
     const colorPicker = document.getElementById("colorPicker");
     colorPicker.onchange = (event) => {
-        app.renderer.backgroundColor = String(event.target.value).replace(/#/, '0X');
+        app.renderer.backgroundColor = String(event.target.value).replace(/#/, "0X");
     };
 
     resetBtn.onclick = () => {
@@ -81,7 +80,7 @@ function init() {
 
 function setupIdolList(idolInfo) {
     const idolList = document.getElementById("idolList");
-    let idolId = 1, idolName = 'mano';
+    let idolId = 1, idolName = "mano";
     idolList.innerHTML = "";
 
     idolInfo.forEach((element, index) => {
@@ -103,7 +102,7 @@ function setupIdolList(idolInfo) {
 
 function testAndLoadDress(idolId, idolName) {
     if (!apiLoader.resources[idolName]) {
-        apiLoader.add(idolName, `https://api.shinycolors.moe/spine/dressList?idolId=${idolId}`).load(function(loader, resources) {
+        apiLoader.add(idolName, `https://api.shinycolors.moe/spine/dressList?idolId=${idolId}`).load(function (loader, resources) {
             setupDressList(JSON.parse(resources[idolName].data));
         });
     }
@@ -116,7 +115,7 @@ function setupDressList(idolDressList) {
     const dressList = document.getElementById("dressList");
     dressList.innerHTML = "";
 
-    let lastType = "P_SSR", optGroup = document.createElement('optgroup');
+    let lastType = "P_SSR", optGroup = document.createElement("optgroup");
     optGroup.label = "P_SSR";
 
     idolDressList.forEach((element, index) => {
@@ -125,7 +124,7 @@ function setupDressList(idolDressList) {
                 dressList.appendChild(optGroup);
             }
             lastType = element.dressType;
-            optGroup = document.createElement('optgroup');
+            optGroup = document.createElement("optgroup");
             optGroup.label = element.dressType;
         }
         let option = document.createElement("option");
@@ -203,40 +202,15 @@ function setupTypeList(dressObj) {
         const dressList = document.getElementById("dressList");
         dressType = typeList.value;
 
-        if (dressType == SML0 || dressType == SML1) {
-            toggleTrackSelectList(false);
-        }
-        else {
-            toggleTrackSelectList(true);
-        }
-
         testAndLoadAnimation(dressList.options[dressList.selectedIndex].getAttribute("dressUUID"), dressType);
     };
 
     testAndLoadAnimation(dressObj.dressUuid, dressType);
 }
 
-function toggleTrackSelectList(toggle) {
-    const animationList2 = document.getElementById("animationList2"),
-        animationList3 = document.getElementById("animationList3"),
-        animationList4 = document.getElementById("animationList4");
-    if (!toggle) { // disable
-        for (let k of [animationList2, animationList3, animationList4]) {
-            k.classList.add("bg-secondary");
-            k.disabled = true;
-        }
-    }
-    else { //enable
-        for (let k of [animationList2, animationList3, animationList4]) {
-            k.classList.remove("bg-secondary");
-            k.disabled = false;
-        }
-    }
-}
-
 function testAndLoadAnimation(uuid, type) {
     if (!app.loader.resources[`${uuid}/${type}`]) {
-        app.loader.add(`${uuid}/${type}`, `https://static.shinycolors.moe/spines/${uuid}/${type}/data.json`).load(function(loader, resources) {
+        app.loader.add(`${uuid}/${type}`, `https://static.shinycolors.moe/spines/${uuid}/${type}/data.json`).load(function (loader, resources) {
             setupAnimationList(resources[`${uuid}/${type}`].spineData);
         });
     }
@@ -246,85 +220,85 @@ function testAndLoadAnimation(uuid, type) {
 }
 
 function setupAnimationList(spineData) {
-    const animationList1 = document.getElementById("animationList1"),
-        animationList2 = document.getElementById("animationList2"),
-        animationList3 = document.getElementById("animationList3"),
-        animationList4 = document.getElementById("animationList4");
-    const defaultAnimation = "wait", noAnimation = "none";
+    const animationList = document.getElementById("divAnimationBody");
+    animationList.innerHTML = "";
 
-    let currentSpine = new PIXI.spine.Spine(spineData);
+    const defaultAnimation = "wait";
+
+    let currentSpine = new PIXI.spine.Spine(spineData),
+        hasWait = false;
 
     try {
-        currentSpine.skeleton.setSkinByName('normal');
+        currentSpine.skeleton.setSkinByName("normal");
     } catch (e) {
-        currentSpine.skeleton.setSkinByName('default');
+        currentSpine.skeleton.setSkinByName("default");
     }
 
-    if (currentSpine.state.hasAnimation(defaultAnimation)) {
-        currentSpine.state.setAnimation(0, defaultAnimation, true);
+    for (let [index, animation] of (spineData.animations).entries()) {
+        const div = document.createElement("div"),
+            input = document.createElement("input"),
+            label = document.createElement("label");
+        const name = animation.name;
+
+        input.setAttribute("type", "checkbox");
+        input.setAttribute("name", name);
+        input.setAttribute("id", name);
+        input.setAttribute("trackNo", index);
+        input.classList.add("form-check-input", "animationElement");
+
+        if (name == defaultAnimation) {
+            input.setAttribute("checked", true);
+            currentSpine.state.setAnimation(index, defaultAnimation, true);
+            hasWait = true;
+        }
+
+        input.addEventListener('change', (e) => {
+            animationOnChange(input, index, currentSpine);
+        });
+
+        label.setAttribute("for", name);
+        label.textContent = name;
+        label.classList.add("form-check-label");
+
+        div.appendChild(input);
+        div.appendChild(label);
+
+        div.classList.add("col-md-4", "col-lg-3", "col-sm-6", "form-check");
+        animationList.appendChild(div);
     }
-    else {
+
+    if (!hasWait) {
+        document.getElementById(currentSpine.spineData.animations[0].name).checked = true;
         currentSpine.state.setAnimation(0, currentSpine.spineData.animations[0].name, true);
     }
-
-    for (let list of [animationList1, animationList2, animationList3, animationList4]) {
-        list.innerHTML = "";
-
-        if (list != animationList1) {
-            const de = document.createElement("option");
-            de.textContent = noAnimation;
-            de.value = noAnimation;
-            de.selected = true;
-            list.appendChild(de);
-        }
-
-        for (let animation of spineData.animations) {
-            const name = animation.name;
-            const option = document.createElement("option");
-            option.textContent = name;
-            option.value = name;
-            option.selected = (name === defaultAnimation) && (list == animationList1);
-            list.appendChild(option);
-        }
-
-    }
-
-    animationOnChange(animationList1, 0, currentSpine);
-    animationOnChange(animationList2, 1, currentSpine);
-    animationOnChange(animationList3, 2, currentSpine);
-    animationOnChange(animationList4, 3, currentSpine);
 
     renderToStage(currentSpine);
 }
 
-function animationOnChange(list, trackNo, currentSpine) {
-    list.onchange = () => {
-        console.log(`Changing track No. ${trackNo}`);
-        let newAnimation = list.value;
-        if (newAnimation == "none") {
-            currentSpine.state.clearTrack(trackNo);
-        }
-        else {
-            currentSpine.state.setAnimation(trackNo, newAnimation, true);
-        }
-        currentSpine.skeleton.setToSetupPose();
-        currentSpine.update(0);
-        currentSpine.autoUpdate = true;
+function animationOnChange(theInput, trackNo, currentSpine) {
+    if (theInput.checked) {
+        const theAnimation = theInput.getAttribute("name");
+        currentSpine.state.setAnimation(trackNo, theAnimation, true);
     }
+    else {
+        currentSpine.state.clearTrack(trackNo);
+    }
+    currentSpine.skeleton.setToSetupPose();
+    currentSpine.update(0);
+    currentSpine.autoUpdate = true;
 }
 
 async function renderByDrop(dataAtlas, dataJson, dataTexture) {
     const rawJson = dataJson;
     const rawAtlas = dataAtlas;
     const rawTexture = await blobToBase64(dataTexture);
-    const spineAtlas = new PIXI.spine.core.TextureAtlas(rawAtlas, (line, callback) => {
+    const spineAtlas = new PIXI.spine.core.TextureAtlas(rawAtlas, (_, callback) => {
         callback(PIXI.BaseTexture.from(rawTexture));
     });
     const spineAtlasLoader = new PIXI.spine.core.AtlasAttachmentLoader(spineAtlas);
     const spineJsonParser = new PIXI.spine.core.SkeletonJson(spineAtlasLoader);
     const spineData = spineJsonParser.readSkeletonData(rawJson);
-    let thisSpine = new PIXI.spine.Spine(spineData);
-    renderToStage(thisSpine);
+    setupAnimationList(spineData);
 }
 
 function blobToBase64(blob) {
@@ -336,7 +310,6 @@ function blobToBase64(blob) {
 }
 
 function renderToStage(currentSpine) {
-
     cont.removeChild(cont.children[0]);
     cont.addChild(currentSpine);
 
@@ -372,16 +345,21 @@ function renderToStage(currentSpine) {
 }
 
 function resetAllAnimation() {
-    const animationList1 = document.getElementById("animationList1"),
-        animationList2 = document.getElementById("animationList2"),
-        animationList3 = document.getElementById("animationList3"),
-        animationList4 = document.getElementById("animationList4");
-
-    animationList1.value = "wait";
-    animationList1.dispatchEvent(new Event("change"));
-
-    for (let k of [animationList2, animationList3, animationList4]) {
-        k.value = "none";
+    let hasWait = false;
+    for (let k of document.getElementsByClassName("animationElement")) {
+        if (k.getAttribute("name") == "wait") {
+            hasWait = true;
+            k.checked = true;
+        }
+        else {
+            k.checked = false;
+        }
         k.dispatchEvent(new Event("change"));
+    }
+
+    if (!hasWait) {
+        const first = document.getElementsByClassName("animationElement")[0];
+        first.checked = true;
+        first.dispatchEvent(new Event("change"));
     }
 }
