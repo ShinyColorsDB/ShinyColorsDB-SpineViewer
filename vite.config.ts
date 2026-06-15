6b6fc3a66979c6f006d5927b4d5219106e7b4348
mitian233 <mitian233@yahoo.co.jp>
2026-06-15 22:13:04 +0800

    Remove launchEditor option from vueDevTools

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   plugins: [
     vue(),
     tailwindcss(),
-    vueDevTools({ launchEditor: 'webstorm' }),
+    vueDevTools(),
     AutoImport({
       imports: [
         'vue',
